3e0e1535583fc1943e6d85d88b744b2fe28d688c
inigoz <inigoz@lodiezda.eus>
2021-02-01 12:15:10 +0100

    mi primer commit

---
+++ php/contacto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="eu">
+<head>
+	<title>Login inprimakia</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+	<link rel="stylesheet" type="text/css" href="estiloak/login_estiloa.css">
+</head>
+<body>
+	<form action="email.php" method="POST" class="">
+		<h1>Kontaktua</h1>
+			<input type="text" name="izena" placeholder="Izena" class="" required="">
+			<input type="text" name="korreoa" placeholder="Korreoa" class="" required="">	
+			<input type="text" name="telefonoa" placeholder="Telefonoa" class="" required="">	
+			<textarea class="formulario__textarea" placeholder="Idatzi zure mezua" id="" name="mezua"></textarea>
+		<input type="submit" name="" value="Bidali" class="btn-bidali">
+		<br>
+	</form>
+</body>
+</html>
